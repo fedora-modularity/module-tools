f3155d789272a7a1e0365d925d7be827bad87c3d
Dominika Hodovska <dhodovsk@redhat.com>
2017-03-30 18:50:15 +0200

    Add build dependencies and variable VERBOSE and CLEAN

--- brt_dep_solver.sh
+++ brt_dep_solver.sh
@@ -1,33 +1,80 @@
-
 #!/bin/bash
 
-pkg=$1
-[ -e brt-provides.txt ] && rm brt-provides.txt
+#set -e
+pkg=${1}
 
-printf "Getting list of base-runtime rpms... "
-wget --quiet -O api.txt  https://github.com/fedora-modularity/base-runtime/blob/master/api.txt?raw=true && echo DONE || exit
+CLEAN=${CLEAN:-true}
+VERBOSE=${VERBOSE:-false}
 
-printf "Modifying list of base-runtime rpms.."
-# get package name of rpms on lines starting with '+', '*', or nothing
-cat api.txt | sed '/^!/ d' | sed $'s/[+*]\t//g' | sed -e "s/-[^-]*-[^-]*$//" > brt-pkgs.txt && echo DONE || exit
+function verbose_echo {
+    [ "$VERBOSE" != "true" ] && return
 
-printf "Getting capabilities provided by base-runtime packages... "
-dnf repoquery --repofrompath brt,https://fedorapeople.org/groups/modularity/repos/base-runtime/26/ --provides -q `paste -s -d ' ' brt-pkgs.txt` > brt-provides.txt && echo DONE || exit
+    echo "$@"
+}
 
-printf "Getting capabilities required by $1... "
-dnf --repofrompath fedora.repo,/etc/yum.repos.d/fedora.repo repoquery --requires -q $pkg > tmp.txt && echo DONE || exit
+# TODO: add other known dependency modules
+function get-baseruntime-caps {
 
-while read cap; do
-  if [[ $cap != *" = "* ]]; then
-    grep -wq "^$cap*" brt-provides.txt || echo $cap >> ${pkg}-caps.txt
-  else
-    grep -wq "$cap" brt-provides.txt || echo $cap >> ${pkg}-caps.txt
-  fi
-done < tmp.txt
+    provided_caps=brt-provides.txt
+    verbose_echo -n "Getting list of base-runtime rpms... "
+    wget --quiet -O api.txt  https://github.com/fedora-modularity/base-runtime/blob/master/api.txt?raw=true && verbose_echo "DONE"
 
-while read cap; do
-  dnf repoquery --whatprovides $cap -q --latest-limit=1 >> cap2rpm.txt
-done < ${pkg}-caps.txt
+    verbose_echo -n "Modifying list of base-runtime rpms.."
+    # get package name of rpms on lines starting with '+', '*', or nothing
+    cat api.txt | sed '/^!/ d' | sed $'s/[+*]\t//g' | sed -e "s/-[^-]*-[^-]*$//" > brt-pkgs.txt && verbose_echo "DONE"
 
-sort -u cap2rpm.txt | sed -e "s/-[^-]*-[^-]*$//" > ${pkg}-deps.txt
+    verbose_echo -n "Getting capabilities provided by base-runtime packages... "
+    dnf repoquery --repofrompath brt,https://fedorapeople.org/groups/modularity/repos/base-runtime/26/ --provides -q `paste -s -d ' ' brt-pkgs.txt` > ${provided_caps} && verbose_echo "DONE"
 
+}
+
+function resolve-deps {
+    if [[ ${1} == "runtime" ]]; then
+      runtime=true
+    elif [[ ${1} == "build" ]]; then
+      runtime=false
+    else
+      echo "resolve-deps: Invalid operation ${1}"
+      exit -1
+    fi
+
+    if [ "${runtime}" = "true" ]; then
+      DIR=${pkg}-runtime-res-files
+    else
+      DIR=${pkg}-build-res-files
+    fi
+
+    mkdir ${DIR}
+    cd ${DIR}
+
+    get-baseruntime-caps
+
+    verbose_echo -n "Getting ${1} capabilities required by ${pkg}... "
+    # TODO: use remote path for fedora repository
+    SRPM_FLAG=
+    [ "${runtime}" = "true" ] || SRPM_FLAG=--srpm
+      dnf --repofrompath fedora.repo,/etc/yum.repos.d/fedora.repo repoquery $SRPM_FLAG --requires -q ${pkg} > ${pkg}-caps.txt && verbose_echo "DONE"
+    while read cap; do
+      if [[ $cap != *" = "* ]]; then
+        grep -wq "^${cap}*" ${provided_caps} || echo ${cap} >> ${pkg}-filtered-caps.txt
+      else
+        grep -wq "${cap}" ${provided_caps} || echo ${cap} >> ${pkg}-filtered-caps.txt
+      fi
+    done < ${pkg}-caps.txt
+
+    while read cap; do
+      dnf repoquery --whatprovides ${cap} -q --latest-limit=1 >> cap2rpm.txt
+    done < ${pkg}-filtered-caps.txt
+
+    if [ "$runtime" = "true" ]; then
+      sort -u cap2rpm.txt | sed -e "s/-[^-]*-[^-]*$//" > ../${pkg}-runtime-deps.txt
+    else
+      sort -u cap2rpm.txt | sed -e "s/-[^-]*-[^-]*$//" > ../${pkg}-build-deps.txt
+    fi
+
+    cd ..
+    ${CLEAN} && rm ${DIR}/* && rmdir ${DIR}
+}
+
+resolve-deps "build"
+resolve-deps "runtime"
